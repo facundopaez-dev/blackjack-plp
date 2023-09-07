43fe6f1e162c1ff42e90d14668a6bec226cf94dc
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-09-07 02:01:21 -0300

    Reglas para la mano suave implementadas en el archivo basicstrategy.pl

--- src/basicstrategy.pl
+++ src/basicstrategy.pl
@@ -4,18 +4,21 @@
 % D - Dealer's card value
 % A - Action (hit, stand, double, split)
 
-basic_strategy(H, D, A) :-
-  % Se debe implementar la estrategia según mano suave o dura.
-  basic(hard, H, D, A).
+basic_strategy(H, D, A) :- basic(hard, H, D, A).
+basic_strategy(H, D, A) :- basic(soft, H, D, A).
 
 basic(hard, H, _, stand) :- H > 16.
 basic(hard, H, _, hit)   :- H < 9.
 basic(hard, H, D, Action) :- basic_hard(H, D, Action).
 
-basic(soft, _, _, no_implementada). % implementar la estrategia suave.
+basic(soft, H, _, stand) :- H > 19.
+basic(soft, H, _, hit) :- H < 18.
+basic(soft, H, D, Action) :- basic_soft(H, D, Action).
 
 :- 
   csv_read_file('basic_hard.csv', Rows, [functor(basic_hard), arity(3)]),
   maplist(assert, Rows).
-  % Construir basic_soft en función del archivo basic_soft.csv
 
+:- 
+  csv_read_file('basic_soft.csv', Rows, [functor(basic_soft), arity(3)]),
+  maplist(assert, Rows).
